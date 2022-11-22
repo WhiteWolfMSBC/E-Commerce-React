528acf892bdca0f03ea258d0e22fb2162a709fee
Sachin Sourabh <Sachin.Sourabh@MSBC-MAINFRAME.LCL>
2022-11-22 19:12:29 +0530

    Updates

---
+++ precache-manifest.9f3a1bcd1c7fd338d3d3943ffc24eddf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a060a383367ef0676a3cfae5dbeb27e",
+    "url": "/E-Commerce-React/index.html"
+  },
+  {
+    "revision": "c6429155ee97e0d0ef89",
+    "url": "/E-Commerce-React/static/css/2.d4dfd184.chunk.css"
+  },
+  {
+    "revision": "9fa88134b4ad479df3c5",
+    "url": "/E-Commerce-React/static/css/main.a9387ce4.chunk.css"
+  },
+  {
+    "revision": "c6429155ee97e0d0ef89",
+    "url": "/E-Commerce-React/static/js/2.943bb9eb.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/E-Commerce-React/static/js/2.943bb9eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fa88134b4ad479df3c5",
+    "url": "/E-Commerce-React/static/js/main.a22f3c80.chunk.js"
+  },
+  {
+    "revision": "61af9fbab7340be43f00",
+    "url": "/E-Commerce-React/static/js/runtime-main.b36e1e38.js"
+  }
+]);
